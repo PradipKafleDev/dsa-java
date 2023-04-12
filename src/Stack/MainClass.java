043ee60be57ce01d50fafaeb2e0e32b21dee1754
PradipKafleDev <pradipkafle074@gmail.com>
2023-04-12 21:07:09 +1000

    Implementaion of stack using array

---
+++ src/Stack/MainClass.java
@@ -0,0 +1,16 @@
+package Stack;
+
+public class MainClass {
+    public static void main(String[] args) throws Exception {
+        StackUsingArray stack = new StackUsingArray(3);
+        stack.push(2);
+        stack.push(4);
+        stack.push(1);
+        stack.push(8);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+    }
+}
---
+++ src/Stack/StackUsingArray.java
@@ -0,0 +1,42 @@
+package Stack;
+
+public class StackUsingArray {
+
+    int a[];
+    int capacity;
+    int top;
+    public StackUsingArray(int capacity){
+        this.capacity=capacity;
+        a= new int[capacity];
+        top=-1;
+
+    }
+    boolean push(int element){
+        if(top==capacity-1) return false;
+        top++;
+        a[top]=element;
+        return true;
+
+    }
+    int pop() throws Exception {
+        if(top== -1) throw  new Exception("Stack is Empty");
+        int ans = a[top];
+        top--;
+        return ans;
+
+    }
+
+    int peak() throws Exception {
+        if(top==1) throw new Exception("Stack is Empty");
+        return a[top];
+
+    }
+
+    boolean isEmpty(){
+        return top==-1;
+    }
+
+    int size(){
+        return top+1;
+    }
+}
