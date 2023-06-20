3ca1bba6bae42b7e280d995367c02cb8d2fea08d
Pradip Kafle <pradip.kafle@zepben.com>
2023-06-20 21:44:24 +1000

    recursion realted problems

--- src/recursion/Basic.java
+++ src/recursion/Basic.java
@@ -49,11 +49,23 @@ public class Basic {
     }
 
 
+    //Time complexity--> 2^n -1
+    static void towerOfHanoi(int n, char from, char to, char aux) {
+        if (n == 0) return;
+
+        towerOfHanoi(n - 1, from, aux, to);
+        System.out.println("Move " + n + " from " + from + " to " + to);
+        towerOfHanoi(n - 1, aux, to, from);
+
+    }
+
+
     public static void main(String[] args) {
 //       int ans =  printSumNaturalNumber(5);
 //       int ans = powerOfNumber(2,5);
-        Set<String> ans = generateSubsets("abc");
-        System.out.println(ans);
+//        Set<String> ans = generateSubsets("abc");
+
+        towerOfHanoi(3, 'A', 'C', 'B');
 
 
     }
